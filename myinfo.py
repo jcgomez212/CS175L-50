88aa7c2f5fd7291161398ccd7a908f1e1b5bd970
jcgomez212 <91210626+jcgomez212@users.noreply.github.com>
2022-01-24 19:10:11 -0500

    myinfo version 3

--- myinfo.py
+++ myinfo.py
@@ -1,6 +1,70 @@
-#CS175-50
-#Juliana Gomez
-#myInfo
-print ("My name is Juliana Gomez")
-print ("I live at 1313 Mockingbird Lane")
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("My name is Juliana Gomez")
+My name is Juliana Gomez
+>>> print (My name is Juliana Gomez)
+SyntaxError: invalid syntax
+>>> print ('My name is Juliana Gomez')
+My name is Juliana Gomez
+>>> anything in quotes is called aa string
+SyntaxError: invalid syntax
+>>> print ('My name is Juliana Gomez")
+       
+SyntaxError: EOL while scanning string literal
+>>> print
+<built-in function print>
+>>> print()
 
+>>> prnit(20)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    prnit(20)
+NameError: name 'prnit' is not defined
+>>> print (20)
+20
+>>> print ("20")
+20
+>>> print (20 + 20)
+40
+>>> print ("20" + "20")
+2020
+>>> print ("camel" + "back")
+camelback
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> junk = "I am a student"
+>>> print ("My name is Juliana Gomez")
+My name is Juliana Gomez
+>>> print (junk)
+I am a student
+>>> junk = "I am a student"
+>>> mail = "at Monmouth University"
+>>> print ("My name is Juliana Gomez")
+My name is Juliana Gomez
+>>> print (junk + mail)
+I am a studentat Monmouth University
+>>> 
+================================================================= RESTART: /Users/julianagomez/Desktop/CS175L-50 Python Codes/myinfo.py =================================================================
+My name is Juliana Gomez
+I am a student
+>>> 
+= RESTART: /Users/julianagomez/Desktop/CS175L-50 Python Codes/myinfo.py
+My name is Juliana Gomez
+I live at 1313 Mockingbird Lane
+>>> 
+= RESTART: /Users/julianagomez/Desktop/CS175L-50 Python Codes/myinfo.py
+My name is Juliana Gomez
+717 Gavlin Ave, Roselle, NJ, 07203
+551-225-4019
+Computer Science B.S.
+>>> 
+= RESTART: /Users/julianagomez/Desktop/CS175L-50 Python Codes/myinfo.py
+My name is Juliana Gomez
+My address is 717 Gavlin Ave, Roselle, NJ, 07203
+My telephone number is 551-225-4019
+My major is Computer Science B.S.
+>>> 
